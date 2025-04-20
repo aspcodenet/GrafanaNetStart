80be81c9aca8e51bc2d92d98b57d38326a4a8195
stefan <stefan.holmberg@systementor.se>
2025-04-20 16:06:03 +0200

    grafana

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,4 +1,48 @@
+using Grafana.OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var serviceName = builder.Configuration["OpenTelemetry:ServiceName"];
+Environment.SetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES", builder.Configuration["OpenTelemetry:Attributes"]);
+Environment.SetEnvironmentVariable("OTEL_SERVICE_NAME", serviceName);
+Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", builder.Configuration["OpenTelemetry:OltpEndpoint"]);
+Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_PROTOCOL", builder.Configuration["OpenTelemetry:Protocol"]);
+
+
+String apiKey = builder.Configuration["OpenTelemetry:ApiToken"];
+String instanceId= builder.Configuration["OpenTelemetry:InstanceID"];
+String auth = instanceId+ ":" + apiKey;
+String base64Auth = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(auth));
+
+
+Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS", "Authorization=Basic " + base64Auth);
+
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(configure =>
+    {
+        configure.UseGrafana().AddAspNetCoreInstrumentation().
+        AddOtlpExporter();
+    })
+    .WithMetrics(configure =>
+    {
+        configure.UseGrafana().AddAspNetCoreInstrumentation().
+        AddPrometheusExporter().
+        AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel")
+            .AddOtlpExporter();
+    });
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.UseGrafana().
+        AddOtlpExporter();
+});
+
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,6 +57,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 var summaries = new[]
 {
@@ -21,11 +66,14 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
+    app.Logger.LogInformation("Starting weather");
     Thread.Sleep(Random.Shared.Next(10, 1000));
     if (Random.Shared.Next(0, 100) > 80)
     {
+        app.Logger.LogError("Something went wrong");
         throw new Exception("Stupid Error");
     }
+    app.Logger.LogInformation("SQL was SELECT * FROM WHATEVER");
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
